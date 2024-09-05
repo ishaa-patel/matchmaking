47c39e44a3f365b542019acf2d6035122e60fa19
Isha <ishapatel2401@gmail.com>
2024-09-05 16:06:24 +0530

    remove splashScreen configuration

--- android/app/src/main/java/com/matchmaking/MainActivity.kt
+++ android/app/src/main/java/com/matchmaking/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.matchmaking
 
-// import android.os.Bundle;
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-// import org.devio.rn.splashscreen.SplashScreen; 
 
 class MainActivity : ReactActivity() {
 
@@ -14,11 +12,6 @@ class MainActivity : ReactActivity() {
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "matchmaking"
-//   @Override
-//       protected void onCreate(Bundle savedInstanceState) {
-//           SplashScreen.show(this);  // here
-//           super.onCreate(savedInstanceState);
-//       }
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
