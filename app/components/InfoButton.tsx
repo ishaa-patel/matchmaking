ae169bd21ab3a6a26d153d6bacae33d50f3aec1e
Isha <ishapatel2401@gmail.com>
2024-09-05 16:14:14 +0530

    changed view to touchabaleOpacity

--- app/components/InfoButton.tsx
+++ app/components/InfoButton.tsx
@@ -1,14 +1,14 @@
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import React from 'react';
 import { styles } from './styles/InfoButtonStyle';
-const InfoButton = ({ label, icon, style, bgColor }: any) => {
+const InfoButton = ({ label, icon, style, bgColor, txtColor }: any) => {
     return (
-        <View style={[styles.btn, { borderColor: style, backgroundColor: bgColor }]} >
+        <TouchableOpacity style={[styles.btn, { borderColor: style, backgroundColor: bgColor }]} >
             {icon}
-            <Text style={styles.btnTxt}>
+            <Text style={[styles.btnTxt, { color: txtColor }]}>
                 {label}
             </Text>
-        </View>
+        </TouchableOpacity>
     );
 };
 export default InfoButton;
